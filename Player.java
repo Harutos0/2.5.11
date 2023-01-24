b1533b61be7ba687797091ad11c78a90d92f72e9
Haruto Suzuki <105263729+Harutos0@users.noreply.github.com>
2023-01-24 13:05:43 -0800

    Add files via upload

--- Player.java
+++ Player.java
@@ -1,3 +1,44 @@
-class Player {
+import java.util.Random;
+
+public class Player {
+
+  private String name;
+  private int score;
+  public static int activePlayer;
+
+  public Player(String n){
+    this.name = n;
+    this.score = 0;
+  }
+
+  public void setName(String n){
+    this.name = n;
+  }
+
+  public String getName(){
+    return this.name;
+  }
+
+  public void setScore(int increment){
+    this.score += increment;
+  }
+
+  public int getScore(){
+    return this.score;
+  }
+
+  public static void switchPlayer(){
+    if (activePlayer == 1) {
+      activePlayer = 2;
+    }
+    else {
+      activePlayer = 1;
+    }
+  }
+
+  public static void choosePlayer(){
+    activePlayer = (int) (Math.random() * 2);
+    System.out.println(activePlayer);
+  }
 
 }
--- nimRunner.java
+++ nimRunner.java
@@ -1,5 +1,11 @@
-class nimRunner {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+public class nimRunner {
+    public static void main(String[] args) {
+
+        Player p1 = new Player("mana");
+        
+        Player.choosePlayer();
+        Player.choosePlayer();
+        Player.choosePlayer();
+        Player.choosePlayer();
   }
 }
